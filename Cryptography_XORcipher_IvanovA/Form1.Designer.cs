364efef87994e243107bc90d6d1e6f608d26b8ac
san2000ches <san2012ches@gmail.com>
2021-04-29 13:56:52 +0300

    Добавьте файлы проекта.

---
+++ Cryptography_XORcipher_IvanovA/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace PR2IvanovAGBBO0118Gammirovanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Enter some text here";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 88);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Password";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 130);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(126, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 56);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Move to the encryption line";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 187);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "ASCII";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 210);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.Text = "UNICODE";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Extra options:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(179, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 40);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 239);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "XORcipher Algotihm by IvanovA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Cryptography_XORcipher_IvanovA/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR2IvanovAGBBO0118Gammirovanie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int sizeOfChar = 8;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true) sizeOfChar = 16;
+            else { sizeOfChar = 8; }
+            string result = "";
+            string mainstr = textBox1.Text;
+            string key = KeyToRightSize(mainstr.Length, textBox2.Text);
+            //MessageBox.Show(key);
+            MessageBox.Show(mainstr.Length.ToString());
+            for(int i=0; i<mainstr.Length; i++)
+            {
+                    result += FunctionPlus(StringToBinaryHalf(key[i], 0), StringToBinaryHalf(mainstr[i], 0));
+                    result += FunctionPlus(StringToBinaryHalf(key[i], ((sizeOfChar / 2))), StringToBinaryHalf(mainstr[i], (sizeOfChar / 2)));
+            }
+            //MessageBox.Show(result);
+            textBox3.Text = BinaryToString(result);
+        }
+        private string FunctionPlus(string inputKey, string input)
+        {
+            //MessageBox.Show(inputKey + "||" + input);
+            string result = "";
+            {
+                for(int i=0; i < input.Length; i++)
+                {
+                    if((inputKey[i] == '1' && input[i] == '1') || (inputKey[i] == '0' && input[i] == '0'))
+                    {
+                        result = result + "0" ;
+                    } 
+                    else
+                    {
+                        result = result + "1";
+                    }
+                }
+            }
+            //MessageBox.Show(result);
+            return result;
+        }
+        private string StringToBinaryHalf(char input, int q)
+        {
+            string res_binary = Convert.ToString(input, 2);
+            while (res_binary.Length < sizeOfChar)
+            {
+                res_binary = "0" + res_binary; 
+            }
+            //res_binary = res_binary.Substring(q, (sizeOfChar / 2));
+            return StringGetHalf(res_binary, q);
+        }
+        private string StringGetHalf (string input, int q)
+        {
+            input = input.Substring(q, (sizeOfChar / 2));
+            return input;
+        }
+        private string BinaryToString (string input)
+        {
+            string output = "";
+            while (input.Length > 0)
+            {
+                string char_binary = input.Substring(0, sizeOfChar);
+                input = input.Remove(0, sizeOfChar);
+                int a = 0;
+                int degree = char_binary.Length - 1;
+                foreach (char c in char_binary)
+                    a += Convert.ToInt32(c.ToString()) * (int)Math.Pow(2, degree--);
+                output += ((char)a).ToString();
+            }
+            return output;
+        }
+        private string KeyToRightSize(int length, string input)
+        {
+            int q = 0;
+            while (q == 0)
+            {
+                if (input.Length >= length) 
+                { 
+                    input = input.Substring(0, length);
+                    q = 1;
+                }
+                if (input.Length < length)
+                {
+                    input += input;
+                }
+            }
+            return input;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox3.Text;
+            textBox3.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Realization of XORcipher encryption algorithm by IvanovA (2021)\n\n- How to encrypt? \nFirst, you need to enter the text you want to encrypt in the top line. " +
+                   "After that, select the key for encryption and press the 'Encrypt' button. You will get the encrypted text in the bottom line. If you need to use more keys, you can Move the encrypted text by pressing the relevant button and encrypt text again." +
+                   "\nP.S. You can also choose encoding type (Unicode or ASCII)." +
+                   "\n\n- How to Decrypt?" +
+                   "\nSimilar to the encryption method. Enter the encrypted text in the top line, necessary key in the middle line and click the 'Encrypt' button. " +
+                   "Order of entering keys, if there were several of them, is not important. You will get the decrypted text in the bottom line.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
